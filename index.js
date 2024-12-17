2ade58c560105df4b62fae17d88038a95f977204
Abhishek <abhishekmeena@Abhisheks-MacBook-Pro-6.local>
2024-12-17 11:13:46 +0530

    intial

--- index.js
+++ index.js
@@ -7,25 +7,57 @@ let numberArray = [];
 
 // GET:
 app.get('/sum', (req, res) => {
-    // write the get api logic here 
+    const {num , arr} = req.body
+    if (!num || !arr.length === 0){
+        return res.status(200).json({message:"arr is empty"})
+    } 
 });
 
 // POST: 
 app.post('/add', (req, res) => {
-    // write the post api logic logic here 
+    const {num , arr} = req.body;
+    try{
+        if(!num){
+            return res.status(400).json({message:"missing number field"})
+        }
+        if (arr){
+            return res.status(200).json({arr:arr})
+        }
+        if(num === ""){
+            return res.status(400).json({message:"Invalid input num should be a number "})
+        }
+    }catch(err){
+        console.error(err)
+        return res.status(500).json({message:"Internal server errror"})
+    }
 });
 
 
 // PUT:
 app.put('/update', (req, res) => {
-    // write the put api logic here 
+    const {num , index} = req.body;
+    if(!num || !index.length ===5){
+        return res.status(400).json({message:"Invalid input Ensure num is a number and index is valid"})
+    } 
+    if(index===0){
+        return res.status(200).json({message:"arr is updated"})
+    }
+    index.push(numberArray)
 });
 
 
 // DELETE: 
 app.delete('/delete/:index', (req, res) => {
-
-    // write the delete api logic here.
+    const {arr} = req.body;
+    if(arr === arr.isEmpty){
+        return res.status(200).json({message:"arr is deleted"})
+    }
+    try{
+        const {num , index} = req.body;
+        if(!num || !index){
+            return res.status(400).json({error:""})
+        }
+    }
 });
 
 
