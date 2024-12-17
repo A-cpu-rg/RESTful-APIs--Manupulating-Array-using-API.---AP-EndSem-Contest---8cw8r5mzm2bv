c0a3be02bd1341a7ac10564f4853bd0f059b471a
Abhishek <abhishekmeena@Abhisheks-MacBook-Pro-6.local>
2024-12-17 11:16:58 +0530

    again

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.delete('/delete/:index', (req, res) => {
     try{
         const {num , index} = req.body;
         if(!num || !index){
-            return res.status(400).json({error:""})
+            return res.status(400).json({error:"Invalid index"})
         }
     }
 });
